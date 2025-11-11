c53c351ec8ea5bd0a9a48f98b277104a6c9f8019
taisan10 <tarunbansalsgnr@gmail.com>
2025-11-11 10:14:57 +0530

    first commit

--- Frontend/src/component/Pages/Home/Contact.jsx
+++ Frontend/src/component/Pages/Home/Contact.jsx
@@ -23,11 +23,11 @@ export default function ContactPage() {
     <section id="contact" className="py-16 sm:py-20">
       <Container className="grid gap-8 sm:gap-12 items-start -mt-8 sm:-mt-12 md:-mt-12 lg:-mt-12 -mb-10 sm:-mb-10 md:-mb-18 lg:-mb-10 text-center ">
         <div>
-          <h3 className="text-xl sm:text-4xl lg:text-4xl  font-semibold leading-tight mb-4 sm:mb-6 text-white/80">
+          <h3 className="text-xl sm:text-4xl lg:text-4xl  font-semibold leading-tight mb-4 sm:mb-5 text-white/80">
             Ready to Grow Your Revenue Through Digital Marketing?
           </h3>
 
-          <div className="text-sm sm:text-md lg:mx-25  mb-4 sm:mb-6 text-white/80">
+          <div className="text-sm sm:text-md  lg:text-2xl lg:mx-25  mb-4 sm:mb-6 text-white/80">
             <h2>
            Our greatest satisfaction comes from seeing our client’s business succeed.{" "}
             </h2>{" "}
